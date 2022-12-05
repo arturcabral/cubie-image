dd58de06a8f1b70262be8ab7f591d8d970ed1cba
Artur Cabral <artur@pop-os.localdomain>
2022-12-05 01:38:46 -0300

    camera

---
+++ camera.py
@@ -0,0 +1,14 @@
+if camlist:
+    
+    # Initialize and start camera  
+    cam = pygame.camera.Camera(camlist[0], (640, 480))
+    cam.start()
+    
+    # capturing the single image
+    image = cam.get_image()
+    
+    # saving the image
+    pygame.image.save(image, "filename.jpg")
+    
+else:
+    if camera is not detected the moving to this part
